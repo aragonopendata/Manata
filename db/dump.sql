b485a1db9c7bef042e3e6ef9d67530e6f64e3753
Jaime Crespo <jynus@seoul.local>
2014-09-26 20:32:13 +0200

    new database structure

--- db/dump.sql
+++ db/dump.sql
@@ -1,4 +1,143 @@
-Usage: mysqldump [OPTIONS] database [tables]
-OR     mysqldump [OPTIONS] --databases [OPTIONS] DB1 [DB2 DB3...]
-OR     mysqldump [OPTIONS] --all-databases [OPTIONS]
-For more options, use mysqldump --help
+-- MySQL dump 10.13  Distrib 5.6.19, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: subvenciones
+-- ------------------------------------------------------
+-- Server version	5.6.19-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `beneficiarios`
+--
+
+DROP TABLE IF EXISTS `beneficiarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `beneficiarios` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` text,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=65536 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `concedentes`
+--
+
+DROP TABLE IF EXISTS `concedentes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `concedentes` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` text,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=64 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `emisores`
+--
+
+DROP TABLE IF EXISTS `emisores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `emisores` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` text,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `emisores_todos`
+--
+
+DROP TABLE IF EXISTS `emisores_todos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `emisores_todos` (
+  `id` int(11) NOT NULL,
+  `tipo` int(11) DEFAULT NULL,
+  `text` text CHARACTER SET latin1,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `rangos`
+--
+
+DROP TABLE IF EXISTS `rangos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rangos` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` text,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `subvenciones_concedidas`
+--
+
+DROP TABLE IF EXISTS `subvenciones_concedidas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `subvenciones_concedidas` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `concedente` text,
+  `beneficiario` text,
+  `finalidad` text,
+  `importe` decimal(12,2) DEFAULT NULL,
+  `norma` text,
+  `orden` text,
+  `ejercicio` year(4) DEFAULT NULL,
+  `importe_ejercicio` decimal(12,2) DEFAULT NULL,
+  `modo_concesion` text,
+  `fecha` date DEFAULT NULL,
+  `descripcion` text,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=135517 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `subvenciones_convocadas`
+--
+
+DROP TABLE IF EXISTS `subvenciones_convocadas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `subvenciones_convocadas` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `fecha` date NOT NULL,
+  `seccion` text NOT NULL,
+  `subseccion` text,
+  `rango` text,
+  `emisor` text,
+  `titulo` text NOT NULL,
+  `texto` longtext NOT NULL,
+  `urlpdf` text NOT NULL,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=415 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-09-26 20:28:05
