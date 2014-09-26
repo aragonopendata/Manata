eca1823747d481cc8ead8234b1ba150990e4932b
Rubén Durán Balda <rduranbal@gmail.com>
2014-09-26 23:28:34 +0200

    Consulta para mostrar subvenciones concediras por emisor

--- web/navegador/views.py
+++ web/navegador/views.py
@@ -1,9 +1,17 @@
 from django.views.generic import TemplateView
 from django.template import RequestContext, loader
 from django.http import HttpResponse
+from django.db.models import Sum, Count
+
+from navegador.models import Concedidas
 
 class EmisoresView(TemplateView):
     template_name = 'navegador/emisores.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(EmisoresView, self).get_context_data(**kwargs)
+        context['lista_concesores'] = Concedidas.objects.filter(ejercicio=2013).values('concedente__name').annotate(importe_total=Sum('importe_ejercicio'), num_conexiones=Count('concedente__name')).order_by('-importe_total')
+        return context
 
 class AyudasView(TemplateView):
     template_name = 'navegador/ayudas.html'
