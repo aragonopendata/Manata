77836a97a25b0e9c7c0cba80ee9ce0d313e65162
Rubén Durán Balda <rduranbal@gmail.com>
2014-09-27 11:28:45 +0200

    Arreglar urls

--- web/navegador/urls.py
+++ web/navegador/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
     url(r'^$', Index.as_view(), name='index'),
     url(r'^emisores$', EmisoresView.as_view(), name='emisores'),
     url(r'^ayudas$', AyudasView.as_view(), name='ayudas'),
-    url(r'^ayudas/(?P<emisor>\w+)/$', AyudasView.as_view(), name='ayudas'),
+    url(r'^ayudas/(?P<emisor>\d+)/$', AyudasView.as_view(), name='ayudas'),
     url(r'^beneficiarios$', AyudasBeneficiariosView.as_view(), name='ayudas_beneficiarios'),
     url(r'^convocadas$', ConvocadasView.as_view(), name='convocadas')
 )
